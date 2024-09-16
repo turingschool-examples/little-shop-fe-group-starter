62f8c92c0acb35f1a65f85c6bbe6761f2eb0b5b3
Jim Macur <jim.macur@gmail.com>
2024-09-16 14:19:42 -0600

    refactor: refactors filter merchant by merchant id

--- main.js
+++ main.js
@@ -235,20 +235,14 @@ function addRemoveActiveNav(nav1, nav2) {
 }
 
 function filterByMerchant(merchantId) {
-  // const specificMerchantItems = items.filter((item) => {
-  //   return 
-  // });
-  // return specificMerchantItems
-  const specificMerchantItems = []
-
-  for (let i = 0; i < items.length; i++) {
-    if (items[i].attributes.merchant_id === parseInt(merchantId)) {
-      specificMerchantItems.push(items[i])
-    }
-  }
+  const specificMerchantItems = items.filter((item) => {
+    return item.attributes.merchant_id === parseInt(merchantId)
+  });
 
   return specificMerchantItems
 }
+
+filterByMerchant(1);
 
 function findMerchant(id) {
   let foundMerchant;
